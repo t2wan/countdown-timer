54726734cda5ab1887ce6463a5c2262e5e9545d7
t2wan <43801413+t2wan@users.noreply.github.com>
2021-08-03 11:55:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const daysE1 = document.getElementById('days')
+const hoursE1 = document.getElementById('hours')
+const minsE1 = document.getElementById('mins')
+const secondsE1 = document.getElementById('seconds')
+const newYears = "14 Aug 2021";
+
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+    const totalseconds = (newYearsDate - currentDate)/1000;
+    const days = Math.floor(totalseconds/3600/24);
+    const hours = Math.floor(totalseconds/3600)%24;
+    const minutes = Math.floor(totalseconds/60)%60;
+    const seconds = Math.floor(totalseconds)%60;
+
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = hours;
+    minsE1.innerHTML = minutes;
+    secondsE1.innerHTML = seconds;
+}
+
+countdown();
+
+setInterval(countdown,1000)
